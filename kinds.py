bbd3b30ece093525a33b8055a0d26f4ce427d13c
“windsleigh” <benjamincorderog@gmail.com>
2023-05-20 23:04:51 -0400

    kinds and checking dictionary

---
+++ kinds.py
@@ -0,0 +1,22 @@
+# Dictionaries
+
+kind = {
+    1: "ArriveCheckIn",
+    2: "ServeCheckIn",
+    3: "ExitCheckIn",
+    4: "ArriveSecurity",
+    5: "ServeSecurity",
+    6: "ExitSecurity",
+    7: "ArriveBoarding",
+    8: "ServeBoarding",
+    9: "ExitBoarding",
+    10: "ArrivePlane",
+    11: "BoardPlane",
+    12: "ExitPlane",
+}
+
+checkin = {
+    1: "counter",
+    2: "totem",
+    3: "online",
+}
