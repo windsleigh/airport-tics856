3adb728d760b843e7dc3b950e833b5b3e0afc466
flosaavedra <flosaavedrau@gmail.com>
2023-05-22 22:14:04 -0400

    adding plane files

---
+++ plane/arrive_plane.py
@@ -0,0 +1,19 @@
+from objects.event import Event
+from global_variables import *
+from methods.random_routine import random_routine
+
+def ArrivePlane(FEL, event):
+    global lanes 
+    global plane_queue
+    # Set arrival time
+    event.entity.time = event.clock
+    # Check for free plane station
+    for lane, value in enumerate(lanes):
+        if value == "free":
+            # Set the station server to the passenger
+            event.entity.lane = lane
+            event.entity.status = 'boarding'
+            # Schedule next serve plan event
+            new_board_event = Event(event.clock, kind[11], event.entity)
+            FEL.append(new_board_event)
+            return
---
+++ plane/exit_plane.py
@@ -0,0 +1,33 @@
+from objects.event import Event
+from global_variables import *
+from methods.random_routine import random_routine
+
+def planeLeaving(FEL, event, ):
+    global plane_queue  
+    if len(plane_queue) > 0:
+        queue_event = plane_queue[0]
+        new_serve_clock = queue_event.clock + event.entity.time
+        queue_event.entity.server = event.entity.server
+
+        # Schedule serve checkin event
+        new_serve_event = Event(new_serve_clock, kind[10], queue_event.entity)
+        FEL.append(new_serve_event)
+
+        # Schedule arrive boarding event
+        event.entity.status = 'arriving'
+
+        new_arriving_time = random_routine(event, "arriving")
+        new_arriving_event = Event(new_arriving_time, kind[10], event.entity)
+        FEL.append(new_arriving_event)
+        return
+
+    else:
+        # If queue is empty change server state to free
+        plane_queue[event.entity.server] = "free"
+
+        # Schedule arrive boarding event
+        event.entity.status = 'arriving'
+        new_arriving_time = random_routine(event, "arriving")
+        new_arriving_event = Event(new_arriving_time, kind[10], event.entity)
+        FEL.append(new_arriving_event)
+        return
