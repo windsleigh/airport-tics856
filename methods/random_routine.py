508a721b2bd9376b327fc211e09ec4f770749f9c
flosaavedra <flosaavedrau@gmail.com>
2023-06-06 16:07:14 -0400

    adding some distributions, this is not correct its temporal

--- methods/random_routine.py
+++ methods/random_routine.py
@@ -1,6 +1,6 @@
 import random
 from config import checkin
-
+import numpy as np
 
 def random_routine(event, kind):
     match event.kind:
@@ -8,27 +8,27 @@ def random_routine(event, kind):
         case "ArriveCheckIn":
             if kind == "clock":
                 # time it takes for a new person to arrive to the airport
-                return event.clock + 0.009
+                return event.clock + np.random.gamma(3, 15, size=None)
             if kind == "checkin":
                 # next checkin type
                 return checkin[random.randint(1, 3)]
             if kind == "online":
                 # time it takes for the person to arrive at security from the entrance
-                return event.clock + 0.8
+                return event.clock + np.random.gamma(3, 15, size=None)
         case "ServeCheckIn":
             if kind == "totem":
                 # Time it takes for the person to pass through a totem
-                return event.clock + 0.03
+                return event.clock + np.random.uniform(2,3)
             if kind == "counter":
                 # Time it takes for the person to pass through a counter
-                return event.clock + 0.06
+                return event.clock + np.random.uniform(2,3)
         case "ExitCheckIn":
             if kind == "totem":
                 # time it takes for the person to arrive at security from totem
-                return event.clock + 0.04
+                return event.clock + np.random.gamma(3, 15, size=None)
             if kind == "counter":
                 # time it takes for the person to arrive at security from counter
-                return event.clock + 0.04
+                return event.clock + np.random.gamma(3, 15, size=None)
 
         # Security
         case "ServeSecurity":
